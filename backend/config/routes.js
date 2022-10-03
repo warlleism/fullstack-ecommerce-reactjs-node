a1949a9bce04465d3f3724a03ab85bc5cfb4c900
Warllei Martins <warllei.s.martins@hotmail.com>
2022-10-03 17:56:16 -0300

    correções

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-const db = require('../src/db')
+const db = require('../model/db')
 
 //Rota para listagem de produtos
 routes.get('/listar', async (req, res) => {
